ac83595523b1f36f00a946d651c7190792f3fdf5
Muhammad Adil Javed <Muhammad Adil Javed>
2016-01-12 16:58:28 +0500

    Initial Commit

---
+++ config/secret.js
@@ -0,0 +1,5 @@
+module.exports = {
+  database: 'mongodb://root:abc123@ds039175.mongolab.com:39175/ecommerce',
+  port: 3000,
+  secretKey: "Adil123@"
+}
---
+++ server.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
+var MongoStore = require('connect-mongo/es5')(session);
+var passport = require('passport');
+
+var secret = require('./config/secret');
+var User = require('./models/user');
+
+var app = express();
+
+mongoose.connect(secret.database, function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database")
+  }
+
+});
+
+//Middleware
+app.use(express.static(__dirname + '/public'));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(cookieParser());
+app.use(session({
+  resave : true,
+  saveUninitialized: true,
+  secret: secret.secretKey,
+  store: new MongoStore({url: secret.database, autoReconnect: true})
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next){
+  res.locals.user = req.user;
+  next();
+});
+
+app.engine('ejs', engine);
+app.set('view engine','ejs');
+
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
+app.use(mainRoutes);
+app.use(userRoutes);
+
+
+
+
+// app.get("/", function(req, res){
+//   var name="batman";
+//   res.json("My Name is " + name);
+//
+// })
+
+// app.get('/catname', function(req, res){
+//   res.json('CatName BatMan')
+//
+// })
+app.listen(secret.port, function(err){
+  if (err) throw err;
+  console.log("server is running on " + secret.port);
+});
